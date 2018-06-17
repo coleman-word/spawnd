b3cc9ba2b27a18755ed5e1b1e384f419d3240867
coleman-word <admin606@protonmail.com>
2018-06-16 22:26:57 -0600

    .

---
+++ common/inventory/main.tf
@@ -0,0 +1,112 @@
+variable cluster_prefix {}
+variable ssh_user {}
+variable domain {}
+
+variable master_as_edge {}
+
+variable master_hostnames {
+  type = "list"
+}
+
+variable master_public_ip {
+  type = "list"
+}
+
+variable master_private_ip {
+  type = "list"
+}
+
+variable edge_count {}
+
+variable edge_hostnames {
+  type = "list"
+}
+
+variable edge_public_ip {
+  type = "list"
+}
+
+variable edge_private_ip {
+  type = "list"
+}
+
+variable node_count {}
+
+variable node_hostnames {
+  type = "list"
+}
+
+variable node_public_ip {
+  type = "list"
+}
+
+variable node_private_ip {
+  type = "list"
+}
+
+variable glusternode_count {}
+variable gluster_volumetype {}
+variable extra_disk_device {}
+
+# Generate Ansible inventory (identical for each cloud provider)
+resource "null_resource" "generate-inventory" {
+  # Trigger rewrite of inventory, uuid() generates a random string everytime it is called
+  triggers {
+    uuid = "${uuid()}"
+  }
+
+  # Write master
+  provisioner "local-exec" {
+    command = "echo \"[master]\" > inventory"
+  }
+
+  # output the lists formated
+  provisioner "local-exec" {
+    command = "echo \"${join("\n",formatlist("%s ansible_ssh_host=%s ansible_ssh_user=${var.ssh_user}", var.master_hostnames, var.master_public_ip))}\" >> inventory"
+  }
+
+  # Write edges
+  provisioner "local-exec" {
+    command = "echo \"[edge]\" >> inventory"
+  }
+
+  # only output if master is edge
+  provisioner "local-exec" {
+    command = "echo \"${var.master_as_edge != true ? "" : join("\n",formatlist("%s ansible_ssh_host=%s ansible_ssh_user=${var.ssh_user}", var.master_hostnames, var.master_public_ip))}\" >> inventory"
+  }
+
+  # output the lists formated, slice list to make sure hostname and ip-list have same length
+  # provisioner output can not be empty string - therefore output space when edge_count == 0
+  provisioner "local-exec" {
+    command = "echo \"${var.edge_count == 0 ? " " : join("\n",formatlist("%s ansible_ssh_host=%s ansible_ssh_user=${var.ssh_user}", slice(var.edge_hostnames,0,var.edge_count), var.edge_public_ip))}\" >> inventory"
+  }
+
+  # Write other variables
+  provisioner "local-exec" {
+    command = "echo \"[all:vars]\" >> inventory"
+  }
+
+  provisioner "local-exec" {
+    command = "echo \"nodes_count=${1 + var.edge_count + var.node_count + var.glusternode_count} \" >> inventory"
+  }
+
+  # If cloudflare domain is set, output that domain, otherwise output a nip.io domain (with the first edge ip)
+  provisioner "local-exec" {
+    command = "echo \"domain=${ var.domain }\" >> inventory"
+  }
+
+  # Always output extra disk device
+  provisioner "local-exec" {
+    command = "echo \"extra_disk_device=${var.extra_disk_device}\" >> inventory"
+  }
+
+  # Always output glusternode count
+  provisioner "local-exec" {
+    command = "echo \"glusternode_count=${var.glusternode_count}\" >> inventory"
+  }
+
+  # Always output gluster_volumetype
+  provisioner "local-exec" {
+    command = "echo \"gluster_volumetype=${var.gluster_volumetype}\" >> inventory"
+  }
+}
